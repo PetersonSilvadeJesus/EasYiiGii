f5b70d9bb14d3868292be80ab8be8d624888a63d
Matheus Evangelista Morais <thtmorais@hotmail.com>
2017-08-27 15:43:19 -0300

    First Commit

---
+++ model/form.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $form yii\widgets\ActiveForm */
+/* @var $generator yii\gii\generators\crud\Generator */
+?>
+<?php
+    echo $form->field($generator, 'tableName');
+    echo $form->field($generator,'relNxN');
+?>
